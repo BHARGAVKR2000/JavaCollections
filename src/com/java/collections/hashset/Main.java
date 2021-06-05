61a7d0d4e57bbd0e18a00fb41aee3b35b142ebf2
Bhargav <bhargavkr2000@gmail.com>
2021-06-05 14:29:16 +0530

    added hashset collection

--- src/com/java/collections/hashset/Main.java
+++ src/com/java/collections/hashset/Main.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 public class Main {
     public static void main(String[] args)
     {
-        HashSet<Student> set = new HashSet<Student>();
+        HashSet<Student> set = new HashSet<>();
 
         //Adding elements to HashSet
 
